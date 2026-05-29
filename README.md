748eb775602bb173dbd76090f3518ee5e3436e26
Rompante <a14866@oficina.pt>
2026-05-29 16:02:54 +0000

    criaração da pessoa

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ node server.js
 
 ## Passe do supebase
 Tom@s2010#0923
+
+## Api supebase
+https://cxrdreagioefeyjsrqeu.supabase.co/rest/v1/
+
+## Chave api
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4cmRyZWFnaW9lZmV5anNycWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNTg5MTYsImV4cCI6MjA5NTYzNDkxNn0.zejEHYngzHaNIPFZfcEoHFqwyP_1gtuyCr_YHXdScO4
--- server.js
+++ server.js
@@ -1,33 +1,68 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+
+const { createClient } = require("@supabase/supabase-js");
+
 const app = express();
 
-// o cors permite a ligação local entre o mesmo sistema
 app.use(cors());
 app.use(express.static("public"));
+app.use('/models', express.static('models')); 
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads/");
-    },
-
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + ".jpg");
-    }
-});
-
+const supabase = createClient("https://cxrdreagioefeyjsrqeu.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4cmRyZWFnaW9lZmV5anNycWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNTg5MTYsImV4cCI6MjA5NTYzNDkxNn0.zejEHYngzHaNIPFZfcEoHFqwyP_1gtuyCr_YHXdScO4);");
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// endpoint para guardar um ficheiro/foto no local que foi designado na criação de 'storage' e depois como upload através de multer
-app.post("/upload", upload.single("photo"), (req, res) => {
-    console.log("Foto recebida:", req.file.filename);
+app.post("/upload", upload.single("photo"), async (req, res) => {
+  try {
+    const emotion = req.body.emotion;
+    const file = req.file;
+    const fileName =
+      Date.now() + ".jpg";
+
+    // Upload para storage no supabase
+    const { data, error } =
+      await supabase.storage
+        .from("photos")
+        .upload(fileName,
+          file.buffer,
+          {
+            contentType: "image/jpeg"
+          });
+
+    if (error)
+      throw error;
+
+    // Ligar URL pública
+    const { data: publicUrl } =
+      supabase.storage
+        .from("photos")
+        .getPublicUrl(fileName);
+
+    // Guardar na tabela
+    await supabase
+      .from("students")
+      .insert([{
+        name: "unknown",
+        emotion: emotion,
+        photo_url:
+          publicUrl.publicUrl
+      }]);
+
     res.json({
-        message: "Foto guardada",
-        file: req.file.filename
+      message: "Guardado com sucesso"
     });
+  }
+
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err.message
+    });
+  }
 });
 
 app.listen(3000, () => {
-    console.log("Servidor a correr em http://localhost:3000");
+  console.log("Servidor a correr em http://localhost:3000");
 });
