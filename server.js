a825c4895743528e52c0f203e6236ad6f94eaf20
Rompante <a14866@oficina.pt>
2026-06-08 08:51:54 +0100

    atualização

---
+++ server.js
@@ -0,0 +1,129 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const multer = require("multer");
+
+
+const { createClient } = require("@supabase/supabase-js");
+
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.use('/models', express.static('models'));
+
+
+const supabase = createClient("https://epjayuksahuwyumxxwwy.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwamF5dWtzYWh1d3l1bXh4d3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNTgzMTMsImV4cCI6MjA5NTYzNDMxM30.wLEhbLFnYSMrDzNwu1efEHYYXCXrhwc98GEJM-n5BTY");
+
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage });
+
+
+app.post("/upload", upload.single("photo"), async (req, res) => {
+
+
+    try {
+
+
+        const emotion = req.body.emotion;
+
+
+        const file = req.file;
+
+
+        const fileName =
+
+            Date.now() + ".jpg";
+
+
+        // Upload para storage no supabase
+
+        const { data, error } =
+
+            await supabase.storage
+
+                .from("photos")
+
+                .upload(fileName,
+
+                    file.buffer,
+
+                    {
+
+                        contentType: "image/jpeg"
+
+                    });
+
+
+        if (error)
+
+            throw error;
+
+
+        // Ligar URL pública
+
+        const { data: publicUrl } =
+
+            supabase.storage
+
+                .from("photos")
+
+                .getPublicUrl(fileName);
+
+
+        // Guardar na tabela
+
+        await supabase
+
+            .from("students")
+
+            .insert([{
+
+                name: "unknown",
+
+                emotion: emotion,
+
+                photo_url:
+
+                    publicUrl.publicUrl
+
+
+            }]);
+
+
+        res.json({
+
+            message: "Guardado com sucesso"
+
+        });
+
+
+    }
+
+    catch (err) {
+
+        console.log(err);
+
+        res.status(500).json({
+
+            error: err.message
+
+        });
+
+    }
+
+});
+
+
+app.listen(3000, () => {
+
+    console.log("Servidor a correr em http://localhost:3000");
+
+});
+
